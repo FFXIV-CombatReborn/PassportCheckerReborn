8b061297dd4de35c0b15a31e762a091fd7d912c0
LTS-FFXIV <127939494+LTS-FFXIV@users.noreply.github.com>
2026-03-19 23:35:40 -0500

    Added local precache system for faster CID resolving

--- PassportCheckerReborn/PassportCheckerReborn.cs
+++ PassportCheckerReborn/PassportCheckerReborn.cs
@@ -45,6 +45,7 @@ public sealed class PassportCheckerReborn : IDalamudPlugin
     internal TomestoneService TomestoneService { get; init; }
     internal FFLogsService FFLogsService { get; init; }
     internal CidCache CidCache { get; init; }
+    internal PremadeCidCache PremadeCidCache { get; init; }
     internal BlacklistCache BlacklistCache { get; init; }
     internal PartyFinderManager PartyFinderManager { get; init; }
 
@@ -55,6 +56,7 @@ public sealed class PassportCheckerReborn : IDalamudPlugin
         TomestoneService = new TomestoneService(this);
         FFLogsService = new FFLogsService(this);
         CidCache = new CidCache();
+        PremadeCidCache = new PremadeCidCache();
         BlacklistCache = new BlacklistCache();
         PartyFinderManager = new PartyFinderManager(this);
 
